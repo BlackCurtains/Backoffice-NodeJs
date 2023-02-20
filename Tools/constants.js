3d59ae4fb7ba26fda3378b53ed25042a05a6faa0
BlackCurtains <webalok8206@gmail.com>
2023-02-20 15:08:53 +0530

    Rendered Login Screen

---
+++ Tools/constants.js
@@ -0,0 +1,8 @@
+const ADMIN_PAGES_PATH = './views/backoffice/pages';
+const APP_PORT = 2000;
+const APP_STATIC_FOLDER = './public';
+
+
+module.exports = {
+  ADMIN_PAGES_PATH, APP_PORT, APP_STATIC_FOLDER
+ }
--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 2000
+const getConst = require('./Tools/constants');
+
+const express = require('express');
+const app = express();
+const port = getConst.APP_PORT;
+
+const path = require("path");
+app.set("view engine", "ejs");
+app.set("views", path.resolve(`${getConst.ADMIN_PAGES_PATH}`));
+app.use(express.static(path.resolve(`${getConst.APP_STATIC_FOLDER}`)));
 
 app.get('/', (req, res) => {
  res.send('Hello World!')
-})
+});
+
+app.get('/backoffice-login', (req, res) => {
+ res.render('login');
+});
+app.post('/backoffice-login', (req, res) => {
+ res.render('login');
+});
 
 app.listen(port, () => {
  console.log(`Example app listening on port ${port}`)
